534938dc691ddea2abf7e52e7d55a2f982812ba0
iambasilp <iambasilp@gmail.com>
2024-07-01 19:11:25 +0530

    Product Description Component  Animation adde

--- frontend/src/Components/Private/ DescriptionBox/ DescriptionBox.jsx
+++ frontend/src/Components/Private/ DescriptionBox/ DescriptionBox.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./DescriptionBox.css";
+import ScrollReveal from "scrollreveal";
 
 const DescriptionBox = () => {
+  useEffect(() => {
+    ScrollReveal().reveal(".descriptionbox-nav-box", {
+      delay: 300,
+      distance: "50px",
+      origin: "bottom",
+      duration: 1000,
+      reset: true,
+    });
+
+    ScrollReveal().reveal(".descriptionbox-description p", {
+      delay: 400,
+      distance: "50px",
+      origin: "bottom",
+      duration: 1000,
+      reset: true,
+    });
+  }, []);
+
   return (
     <div className="descriptionbox">
       <div className="descriptionbox-navigator">
@@ -10,23 +29,24 @@ const DescriptionBox = () => {
       </div>
       <div className="descriptionbox-description">
         <p>
-          Welcome to our furniture store, where you can explore a wide range of
-          exquisite pieces designed to enhance your living spaces. Discover
-          high-quality furniture crafted with precision and style, available
-          exclusively online. Our virtual showroom offers a seamless shopping
-          experience, allowing you to browse through our curated collection from
-          the comfort of your home.
+          Welcome to our furniture store, where you can explore a wide range
+          of exquisite pieces designed to enhance your living spaces.
+          Discover high-quality furniture crafted with precision and style,
+          available exclusively online. Our virtual showroom offers a
+          seamless shopping experience, allowing you to browse through our
+          curated collection from the comfort of your home.
         </p>
         <p>
           Each piece in our collection is meticulously selected to blend
-          functionality with aesthetic appeal, catering to diverse tastes and
-          preferences. Whether you're furnishing a cozy corner or redesigning an
-          entire room, our furniture pieces promise durability, comfort, and
-          timeless elegance. Explore detailed product descriptions, stunning
-          imagery, and competitive pricing to make informed decisions. We
-          provide comprehensive information on dimensions, materials, and
-          customization options, ensuring each product page delivers the
-          insights you need to transform your space.
+          functionality with aesthetic appeal, catering to diverse tastes
+          and preferences. Whether you're furnishing a cozy corner or
+          redesigning an entire room, our furniture pieces promise
+          durability, comfort, and timeless elegance. Explore detailed
+          product descriptions, stunning imagery, and competitive pricing to
+          make informed decisions. We provide comprehensive information on
+          dimensions, materials, and customization options, ensuring each
+          product page delivers the insights you need to transform your
+          space.
         </p>
       </div>
     </div>
