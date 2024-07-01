c2a563f7daf50355439affc60056bb3a5b4ded16
iambasilp <iambasilp@gmail.com>
2024-07-01 19:23:33 +0530

    LoaginSignup component Animation adde

--- frontend/src/Pages/LoginSignup/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup/LoginSignup.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
-import './LoginSignup.css'
+import React, { useEffect } from 'react';
+import './LoginSignup.css';
+import ScrollReveal from 'scrollreveal';
+
 const LoginSignup = () => {
+  useEffect(() => {
+    // Initialize ScrollReveal
+    ScrollReveal().reveal('.loginsignup-container', {
+      delay: 200,
+      duration: 1000,
+      easing: 'ease-in-out',
+      origin: 'bottom',
+      distance: '50px',
+      interval: 150,
+    });
+
+    ScrollReveal().reveal('.loginsignup-fields input, .loginsignup-fields button, .loginsignup-agree input, .loginsignup-agree p', {
+      delay: 200,
+      duration: 1000,
+      easing: 'ease-in-out',
+      origin: 'bottom',
+      distance: '30px',
+      interval: 100,
+    });
+
+  }, []);
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
@@ -14,11 +38,11 @@ const LoginSignup = () => {
         <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
         <div className="loginsignup-agree">
           <input type="checkbox" name='' id='' />
-          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
