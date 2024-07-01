15083ab072984bde2b596550e46f47481c778ef1
iambasilp <iambasilp@gmail.com>
2024-07-01 19:21:49 +0530

    Each Item Product Animation adde

--- frontend/src/Components/Shared/Item/Item.jsx
+++ frontend/src/Components/Shared/Item/Item.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import ScrollReveal from "scrollreveal";
 import "./Item.css";
 
 const Item = (props) => {
+  useEffect(() => {
+    ScrollReveal().reveal(`.item-${props.id}`, {
+      delay: 100, // Adjust delay based on item id
+      duration: 800,
+      origin: "bottom",
+      distance: "20px",
+      easing: "ease",
+    });
+  }, [props.id]);
+
   return (
-    <div className="item">
+    <div className={`item item-${props.id}`}>
       <div className="item-img">
-        <a href="#">
-        <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
-        </a>
+        <Link to={`/product/${props.id}`}>
+          <img onClick={() => window.scrollTo(0, 0)} src={props.image} alt="" />
+        </Link>
       </div>
 
       <p>{props.name}</p>
