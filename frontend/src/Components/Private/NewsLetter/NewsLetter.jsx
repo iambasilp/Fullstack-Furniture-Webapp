db4b8632e0c75db2c384c5649cdd6161b8babd55
iambasilp <iambasilp@gmail.com>
2024-07-01 19:02:20 +0530

    NewsLetter Component Animation added

--- frontend/src/Components/Private/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/Private/NewsLetter/NewsLetter.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
 import './NewsLetter.css';
 
 const NewsLetter = () => {
+  useEffect(() => {
+    // Initialize ScrollReveal
+    ScrollReveal().reveal('.newsletter', {
+      duration: 1000,
+      origin: 'bottom',
+      distance: '20px',
+      delay: 300,
+      easing: 'ease',
+    });
+  }, []);
+
   return (
     <div className='newsletter'>
       <h1>Get Exclusive Furniture Offers</h1>
