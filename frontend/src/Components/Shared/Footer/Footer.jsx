d5f979aaff81d7c7859ea9de0b71808be9a0a8f0
iambasilp <iambasilp@gmail.com>
2024-07-01 19:05:10 +0530

    Footer Component  Animation adde

--- frontend/src/Components/Shared/Footer/Footer.jsx
+++ frontend/src/Components/Shared/Footer/Footer.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ScrollReveal from "scrollreveal";
 import "./Footer.css";
 import { RiSofaLine } from "react-icons/ri";
-import { FaInstagram } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram, FaWhatsapp, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
+  useEffect(() => {
+    // Initialize ScrollReveal for the footer elements
+    ScrollReveal().reveal('.footer', {
+      duration: 1000,
+      origin: 'bottom',
+      distance: '20px',
+      delay: 300,
+      easing: 'ease',
+    });
+  }, []);
+
   return (
     <div className="footer">
       <div className="footer-logo">
@@ -41,11 +51,12 @@ const Footer = () => {
           <FaLinkedin className="footer-icon" />
         </div>
       </div>
+
       <div className="footer-copyright">
         <hr />
         <div className="footer-copyright-content">
           <div className="footer-logo-bottom-icon">
-            <RiSofaLine  size={20}/>
+            <RiSofaLine size={20} />
           </div>
           <p>Copyright @ 2024 - All Right Reserved.</p>
         </div>
