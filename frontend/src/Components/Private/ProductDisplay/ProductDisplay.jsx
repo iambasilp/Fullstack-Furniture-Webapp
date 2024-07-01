69057be594badf769ee847d6e665be0996edd6db
iambasilp <iambasilp@gmail.com>
2024-07-01 17:22:07 +0530

    ProductDisplay Component Added

---
+++ frontend/src/Components/Private/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDisplay = () => {
+  return (
+    <div>
+      Product Display here
+    </div>
+  )
+}
+
+export default ProductDisplay
--- frontend/src/Pages/Product/Product.jsx
+++ frontend/src/Pages/Product/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Pathway from '../../Components/Private/Pathway/Pathway';
+import ProductDisplay from '../../Components/Private/ProductDisplay/ProductDisplay';
 
 
 const Product = () => {
@@ -11,6 +12,7 @@ const Product = () => {
   return (
     <div>
       <Pathway product={product}/>
+      <ProductDisplay product={product}/>
     </div>
   )
 }
