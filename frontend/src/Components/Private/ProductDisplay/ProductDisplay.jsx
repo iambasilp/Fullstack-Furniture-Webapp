91ac3bed4d3cad50610a22275db4dd6fb5e426e9
iambasilp <iambasilp@gmail.com>
2024-07-01 17:41:47 +0530

    Property Display Page Added

--- frontend/src/Components/Private/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/Private/ProductDisplay/ProductDisplay.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './ProductDisplay.css'
+import { BsFillStarFill } from "react-icons/bs";
+import { BsStar } from "react-icons/bs";
 
-const ProductDisplay = () => {
+import { ShopContext } from '../../../Context/ShopContext';
+const ProductDisplay = (props) => {
+
+    const {product} = props;
+    const {addToCart} = useContext(ShopContext);
   return (
-    <div>
-      Product Display here
+    <div className='productdisplay'>
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+            <img className='productdisplay-main-img' src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-stars">
+        <BsFillStarFill size={25} />
+        <BsFillStarFill size={25} />
+        <BsFillStarFill size={25} />
+        <BsFillStarFill size={25} />
+        <BsStar size={25} />
+            <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+            <div className="productdisplay-right-price-old">${product.old_price}</div>
+            <div className="productdisplay-right-price-new">${product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+        A lightweight, usually knitted, pullover shirt, close-fitting and with
+          a round neckline and short sleeves, worn as an undershirt or outer
+          garment.
+        </div>
+
+        <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
+        <p className='productdisplay-right-category'><span>Category :</span>Women , T-Shirt, Crop Top</p>
+        <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
+      </div>
     </div>
   )
 }
