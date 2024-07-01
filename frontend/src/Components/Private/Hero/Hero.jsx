04a11e99f301a42a62210e2934658a2336ab7de9
iambasilp <iambasilp@gmail.com>
2024-07-01 18:50:50 +0530

    Hero Section Animation added

--- frontend/src/Components/Private/Hero/Hero.jsx
+++ frontend/src/Components/Private/Hero/Hero.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Hero.css";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import hero_image from "../../../assets/Hero/hero_image.png";
+import ScrollReveal from "scrollreveal";
 
 const Hero = () => {
-    
+  useEffect(() => {
+    ScrollReveal().reveal(".hero-left", {
+      origin: "left",
+      distance: "20px",
+      duration: 1000,
+      delay: 300,
+      easing: "ease",
+    });
+    ScrollReveal().reveal(".hero-right", {
+      origin: "right",
+      distance: "20px",
+      duration: 1000,
+      delay: 300,
+      easing: "ease",
+    });
+  }, []);
+
   return (
     <div className="hero">
       <div className="hero-left">
