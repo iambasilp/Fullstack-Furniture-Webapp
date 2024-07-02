36741f9f33e7932e7d7ba162b8a7fe6b19cdc370
iambasilp <iambasilp@gmail.com>
2024-07-02 16:07:41 +0530

    CartItems UI Completed

--- frontend/src/Components/Private/CartItems/CartItems.jsx
+++ frontend/src/Components/Private/CartItems/CartItems.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
+import "./CartItems.css";
+import { ImCross } from "react-icons/im";
+
 
 const CartItems = () => {
   return (
-    <div>
-      CartItems
-    </div>
-  )
-}
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <div>
+        <div className="cartitems-format cartitems-format-main">
+          <img src="https://i0.wp.com/thinkfirstcommunication.com/wp-content/uploads/2022/05/placeholder-1-1.png?fit=1200%2C800&ssl=1" alt="" className="carticon-product-icon" />
+          <p>name</p>
+          <p>$ 0</p>
+          <button className="cartitems-quantity">0</button>
+          <p>$ 0</p>
+          <ImCross className="cartitems-remove-icon" size={25}/>
+        </div>
+        <hr />
+      </div>
 
-export default CartItems
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>$ 0</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>$ 0</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItems;
