32a3eb5778c95d2023f70ca98b53fecd2d84cd49
iambasilp <iambasilp@gmail.com>
2024-07-02 08:03:34 +0530

    Update: Pass product description data from product page fetch API

--- frontend/src/Components/Private/DescriptionBox/DescriptionBox.jsx
+++ frontend/src/Components/Private/DescriptionBox/DescriptionBox.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import "./DescriptionBox.css";
 import ScrollReveal from "scrollreveal";
 
-const DescriptionBox = () => {
+const DescriptionBox = ({product}) => {
   useEffect(() => {
     ScrollReveal().reveal(".descriptionbox-nav-box", {
       delay: 300,
@@ -25,28 +25,11 @@ const DescriptionBox = () => {
     <div className="descriptionbox">
       <div className="descriptionbox-navigator">
         <div className="descriptionbox-nav-box">Description</div>
-        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        <div className="descriptionbox-nav-box fade">{`Reviews (${product.reviews})`}</div>
       </div>
       <div className="descriptionbox-description">
         <p>
-          Welcome to our furniture store, where you can explore a wide range
-          of exquisite pieces designed to enhance your living spaces.
-          Discover high-quality furniture crafted with precision and style,
-          available exclusively online. Our virtual showroom offers a
-          seamless shopping experience, allowing you to browse through our
-          curated collection from the comfort of your home.
-        </p>
-        <p>
-          Each piece in our collection is meticulously selected to blend
-          functionality with aesthetic appeal, catering to diverse tastes
-          and preferences. Whether you're furnishing a cozy corner or
-          redesigning an entire room, our furniture pieces promise
-          durability, comfort, and timeless elegance. Explore detailed
-          product descriptions, stunning imagery, and competitive pricing to
-          make informed decisions. We provide comprehensive information on
-          dimensions, materials, and customization options, ensuring each
-          product page delivers the insights you need to transform your
-          space.
+         {product.description}
         </p>
       </div>
     </div>
