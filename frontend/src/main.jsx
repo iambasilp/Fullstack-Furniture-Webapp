e492f319d64e1c0839fc258a0e641ceec0f22bfa
iambasilp <iambasilp@gmail.com>
2024-07-03 21:32:37 +0530

    Wrap the application with UserContextProvider in main.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import ShopContextProvider from "./Context/ShopContext.jsx";
+import UserContextProvider from "./Context/UserContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ShopContextProvider>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
     </ShopContextProvider>
   </React.StrictMode>
 );
