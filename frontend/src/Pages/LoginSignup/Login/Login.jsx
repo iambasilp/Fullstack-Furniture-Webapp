a8fb5c763e28be4271a124b3c765827e2f5b8382
iambasilp <iambasilp@gmail.com>
2024-07-04 00:39:04 +0530

    Change connection from Registration to normal to manage context by

--- frontend/src/Pages/LoginSignup/Login/Login.jsx
+++ frontend/src/Pages/LoginSignup/Login/Login.jsx
@@ -1,13 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './Login.css';
 import ScrollReveal from 'scrollreveal';
-import {Link, useNavigate} from 'react-router-dom'
 
 const Login = () => {
-  const [email,setEmail] = useState("")
-  const [password,setPassword] = useState("")
-  const Navigate = useNavigate()
-
   useEffect(() => {
     // Initialize ScrollReveal
     ScrollReveal().reveal('.login-container', {
@@ -29,33 +24,17 @@ const Login = () => {
     });
 
   }, []);
-  const HandleLogin = async()=>{
-    try{
-    const response = await fetch ('http://localhost:3000/users')
-    const users = await response.json()
-    const user = users.find((user)=>user.email===email && user.password===password)
-    if(user){
-      alert("Login Successfully")
-      localStorage.setItem('currentUser',JSON.stringify(user))
-      Navigate("/")
-    }else{
-      alert("Invalid Email or Password")
-    }
-    }catch(error){
-      console.log(error)
-    }
-  }
 
   return (
     <div className='login'>
       <div className="login-container">
         <h1>Login</h1>
         <div className="login-fields">
-          <input type="email" onChange={(e)=>setEmail(e.target.value)} placeholder='Email Address' />
-          <input type="password" onChange={(e)=>setPassword(e.target.value)} placeholder='Password' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
         </div>
-        <button onClick={HandleLogin}>Login</button>
-        <p className="login-signup">Don't have an account? <span><Link to="/signup">Sign up here</Link></span></p>
+        <button>Login</button>
+        <p className="login-signup">Don't have an account? <span>Sign up here</span></p>
       </div>
     </div>
   );
--- frontend/src/Pages/LoginSignup/Signup/Signup.jsx
+++ frontend/src/Pages/LoginSignup/Signup/Signup.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './Signup.css';
 import ScrollReveal from 'scrollreveal';
-import {Link,useNavigate} from 'react-router-dom'
-
 
 const Signup = () => {
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const Navigate = useNavigate()
   useEffect(() => {
     // Initialize ScrollReveal
     ScrollReveal().reveal('.signup-container', {
@@ -30,41 +24,18 @@ const Signup = () => {
     });
 
   }, []);
- const HandleSignUp = async()=>{
-  try{
 
-  
-    const response = await fetch('http://localhost:3000/users',{
-      method:'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify({name,email,password})
-    })
-    if (response.ok) {
-      const data = await response.json()
-      localStorage.setItem('users',JSON.stringify(data))
-      Navigate('/login')
-      alert('Sign up successful!');
-    } else {
-      alert('Sign up failed');
-    }
-  }catch(error){
-    console.log("Error",error)
-  }
-
- }
   return (
     <div className='signup'>
       <div className="signup-container">
         <h1>Sign Up</h1>
         <div className="signup-fields">
-          <input type="text" onChange={(e)=>setName(e.target.value)} placeholder='Your Name' />
-          <input type="email" onChange={(e)=>setEmail(e.target.value)} placeholder='Email Address' />
-          <input type="password" onChange={(e)=>setPassword(e.target.value)} placeholder='Password' />
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
         </div>
-        <button onClick={HandleSignUp}>Continue</button>
-        <p className="signup-login">Already have an account? <span><Link to="/login">Sign up here</Link></span></p>
+        <button>Continue</button>
+        <p className="signup-login">Already have an account? <span>Login here</span></p>
         <div className="signup-agree">
           <input type="checkbox" name='' id='' />
           <p>By continuing, I agree to the terms of use & privacy policy.</p>
