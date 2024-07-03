b5fef570ac263bff1f8f2db9d8e70024ea7ef598
iambasilp <iambasilp@gmail.com>
2024-07-03 23:47:09 +0530

    Implement tag search functionality: Use case-insensitive search to filter items based on tags.

--- frontend/src/Pages/ShopCategory/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory/ShopCategory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./ShopCategory.css";
 import { ProductContext } from "../../Context/ProductContext";
 import { RiArrowDropDownLine } from "react-icons/ri";
@@ -6,44 +6,52 @@ import Item from "../../Components/Shared/Item/Item";
 
 const ShopCategory = (props) => {
   const { AllProductData } = useContext(ProductContext);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredProducts = AllProductData.filter((item) => {
+    if (props.category === item.category) {
+      return item.tags.some((tag) =>
+           tag.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+   return false;
+  });
+
   return (
     <div className="shop-category">
       <div className="shopcategory-indexSort">
         <p>
-          <span>
-            {
-              AllProductData.filter((item) => item.category === props.category)
-                .length
-            }
-          </span>{" "}
-          out of {AllProductData.length} products
+          <span>{filteredProducts.length}</span> out of {AllProductData.length}{" "}
+          products
         </p>
 
-        <div className="shopcategory-sort">
-          <p>Sort by</p>
-          <div className="shopcategory-sort-icon">
-            <RiArrowDropDownLine size={28} />
-          </div>
+        <div className="shopcategory-search">
+          <input
+            type="text"
+            placeholder="Search by tags..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
         </div>
       </div>
+
       <div className="shopcategory-products">
-        {AllProductData.map((item, i) => {
-          if (props.category === item.category) {
-            return (
-              <Item
-                key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-                old_price={item.old_price}
-              />
-            );
-          } else {
-            return null;
-          }
-        })}
+        {filteredProducts.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
+
       <button className="shopcategory-loadmore">Explore More</button>
     </div>
   );
