3bfc71583cab33c3367a4a2a2299872bc17518b9
iambasilp <iambasilp@gmail.com>
2024-07-08 09:36:48 +0530

    Admin Page Created

---
+++ admin/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div>
+      App
+    </div>
+  </BrowserRouter>
+  )
+}
+
+export default App
