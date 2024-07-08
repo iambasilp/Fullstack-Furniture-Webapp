95b643eca487e870882efccf0dbeab43e0053f41
iambasilp <iambasilp@gmail.com>
2024-07-08 10:12:36 +0530

    List Product Component Added

---
+++ admin/src/Components/ProductList/ListProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ListProduct.css'
+const ListProduct = () => {
+  return (
+    <div>
+      List Product
+    </div>
+  )
+}
+
+export default ListProduct
--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -8,16 +8,16 @@ import { Link } from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <Link to='/addproduct' style={{ textDecoration: 'none' }}>
-        <div className="sidebar-item">
-        <FaCartPlus className='product-add' />
-          <p>Add Product</p>
-        </div>
-      </Link>
       <Link to='/listproduct' style={{ textDecoration: 'none' }}>
         <div className="sidebar-item">
         <MdFeaturedPlayList className='product-list' />
           <p>Product List</p>
+        </div>
+      </Link>
+      <Link to='/addproduct' style={{ textDecoration: 'none' }}>
+        <div className="sidebar-item">
+        <FaCartPlus className='product-add' />
+          <p>Add Product</p>
         </div>
       </Link>
       
