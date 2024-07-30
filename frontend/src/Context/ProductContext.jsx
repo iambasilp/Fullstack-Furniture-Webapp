6bc2bbc73ac5e9f2089e99b16c650c5ef5ce7b74
iambasilp <iambasilp@gmail.com>
2024-07-30 23:35:19 +0530

    Product Data Handleed using custome hook useFech

--- frontend/src/Context/ProductContext.jsx
+++ frontend/src/Context/ProductContext.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState, createContext } from "react";
 import { getFetchDataFromApi } from "../DataFetching/FetchData";
+import { useFetch } from "../Hooks/useFetch";
 
 export const ProductContext = createContext(null);
 
 const ProductContextProvider = (props) => {
-  const [AllProductData, setAllProductData] = useState([]);
-
-  useEffect(() => {
-    getFetchDataFromApi().then((data) => setAllProductData(data));
-  }, []);
+  const { data:AllProductData, } = useFetch('http://localhost:3010/products');
+ 
 
   return (
     <ProductContext.Provider value={{ AllProductData }}>
--- frontend/src/DataFetching/FetchData.jsx
+++ frontend/src/DataFetching/FetchData.jsx
@@ -1,13 +1,16 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export const getFetchDataFromApi = async () => {
-    try {
-        const response = await axios.get('http://localhost:3000/products');
-        if (!response.data) {
-            throw new Error('Empty response');
-        }
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-};
+// export const getFetchDataFromApi = async () => {
+//     try {
+//         const response = await axios.get('http://localhost:3010/products');
+//         if (!response.data) {
+//             throw new Error('Empty response');
+//         }
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//     }
+// };
+
+// this was i used first but after i knowing i can create and handle by custome hoook i changed - 
+//  to custome hook Hooks/
