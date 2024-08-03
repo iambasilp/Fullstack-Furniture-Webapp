e60128bfd8fc2af87b9f56a45ada135ee03aba43
iambasilp <iambasilp@gmail.com>
2024-08-03 11:28:56 +0530

    feat: add store and provider to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { store } from "./Redux/Store";
+import { Provider } from "react-redux";
 import ProductContextProvider from "./Context/ProductContext.jsx";
 import CartContextProvider from "./Context/CartContext.jsx";
 import UserContextProvider from "./Context/UserContext.jsx";
@@ -10,13 +12,15 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProductContextProvider>
-        <UserContextProvider>
-          <CartContextProvider>
-            <App />
-          </CartContextProvider>
-        </UserContextProvider>
-      </ProductContextProvider>
+      <Provider store={store}>
+        <ProductContextProvider>
+          <UserContextProvider>
+            <CartContextProvider>
+              <App />
+            </CartContextProvider>
+          </UserContextProvider>
+        </ProductContextProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
